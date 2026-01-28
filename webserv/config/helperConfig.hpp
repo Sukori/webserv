6fc4237b690733e0ae3628f01da8020387404edd
pberset <pberset@student.42lausanne.ch>
2026-01-28 17:40:57 +0100

    default values

--- webserv/config/helperConfig.hpp
+++ webserv/config/helperConfig.hpp
@@ -19,35 +19,36 @@
 # include <sstream>
 # include <vector>
 # include <map>
+# include <utility>
 # include <cctype>
 
 struct s_location {
-	std::string							route; // location "/dir/" {     for instance
-	std::string							root_path; // root "/var/www"; } for instance
-	std::string							alias;
-	std::vector<std::string>			limit_except;
-	bool								autoindex;
-	std::string							upload_path;
-	std::map<std::string, std::string>	cgi_param;
-	std::string							cgi_pass;
+	std::string							route = "/"; // location "/dir/"     for instance
+	std::string							root_path = "/"; // root "/var/www"; for instance
+	std::string							alias = "/";
+	std::vector<std::string>			limit_except = {};
+	bool								autoindex = true;
+	std::string							upload_path = "/uploads/";
+	std::map<std::string, std::string>	cgi_param = {std::string(""), std::string("")};
+	std::string							cgi_pass = "";
 };
 
 
 struct s_listen {
-	/* IPV4 are stored in IPV4 mapped IPV6
-	::ffff:xxxx:xxxx */
-	std::string	ip; 
-	int			port; // 8080++ for this project
+	std::string	ip = "0.0.0.0"; //allways broadcast for this project
+	int			port = 0; // 8080++ for this project
+	std::string	protocol = "HTTP"; //HTTP, TCP, SSL, SSH ...
 };
 
 struct s_server {
-	std::vector<s_listen>		listen;
-	std::string					root;
-	std::vector<std::string>	index;
-	std::string					access_logs;
-	std::string					error_logs;
-	long						client_max_body_size; // convert "10m" or "10M" in bits (10000000)
-	std::map<int, std::string>	error_pages; // {error_code<int>, path_to_page<string>}, {404, "/www/error_pages/404.html"}
+	std::vector<s_listen>		listen = {};
+	std::string					serverName = "";
+	std::string					root = "/";
+	std::vector<std::string>	index = {};
+	std::string					access_logs = "";
+	std::string					error_logs = "";
+	long						client_max_body_size = 1024; // convert "10m" or "10M" in bits (10000000)
+	std::map<int, std::string>	error_pages = {0, std::string("")}; // {error_code<int>, path_to_page<string>}, {404, "/www/error_pages/404.html"}
 };
 
 std::string	readFile(const char* file);
