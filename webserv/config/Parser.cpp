2978d0ece82f2ae65707a77fe3f47b4ebae0fe61
sukori <sukori98@gmail.com>
2026-02-02 19:55:22 +0100

    parse listen

--- webserv/config/Parser.cpp
+++ webserv/config/Parser.cpp
@@ -6,15 +6,30 @@
 /*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 14:36:34 by pberset           #+#    #+#             */
-/*   Updated: 2026/01/28 14:36:45 by pberset          ###   Lausanne.ch       */
+/*   Updated: 2026/02/02 19:54:25 by pberset          ###   Lausanne.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Parser.hpp"
+#include <cmath>
 
 Parser::Parser(const std::string data): _ss(data) {}
 
 Parser::~Parser(void) {}
+
+static int	ft_stoui(std::string& token) {
+	int	output;
+
+	for (unsigned int i = 0; i < token.size(); i++) {
+		char	c = token.at(i);
+		if (c - '0' < 0 && c - '0' > 9) {
+			std::cerr << "ft_stoui: invalid char " << c << std::endl;
+			return (-1);
+		}
+		output += (c - '0') * std::pow(10, (token.size() - i));
+	}
+	return (output);
+}
 
 std::vector<Server> Parser::initParser(void) {
     std::vector<Server> output = {};
@@ -24,7 +39,7 @@ std::vector<Server> Parser::initParser(void) {
 		if (token == "server") {
 			output.push_back(parseServer());
 		} else {
-			std::cerr << "config file: unexpected token at root level: " << token << std::endl;
+			std::cerr << "initParser: unexpected token at root level " << token << std::endl;
 			//set a custom errno and return?
 			break ;
 		}
@@ -46,7 +61,7 @@ Server	Parser::parseServer(void) {
 
 	_ss >> token;
 	if (token != "{") {
-		std::cerr << "server configuration: unexpected token: " << token << std::endl;
+		std::cerr << "parseServer: unexpected token " << token << std::endl;
 		//set a custom errno?
 		return (Server(servStruct,locs));
 	}
@@ -77,13 +92,27 @@ Server	Parser::parseServer(void) {
 
 struct s_listen	parseListen(void) {
 	struct s_listen	output;
+	std::string		token;
+	int				port;
 	//ip and port are the only data.
 	//for simplicity, ip is assumed to be 0.0.0.0 and is not written in config
 	output.ip = "0.0.0.0";
+	_ss >> token;
+	port = ft_stoui(token);
+	if (port < 8000) {
+		//error invalid port
+		std::cerr << "port: invalid port " << port << std::endl;
+		return (output);
+	}
 	//first int for port - else error handling
-	//output.port = token;
-	//then protocol - if token == ';' - then default protocol to 'HTTP'
-	//output.protocol = token;
+	output.port = token;
+	_ss >> token;
+	if (token != ';') {
+		std::cerr << "parseListen: unexpected token " << token << std::endl;
+		//set a custom errno ?
+		output.port = -1;
+	}
+	//output.protocol = token; //actually, we don't accept anything else than "http"
 	return (output);
 }
 
