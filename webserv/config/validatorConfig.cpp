ac11184c8542c593e28ad952e4ffb0212797f3b4
pberset <pberset@student.42lausanne.ch>
2026-03-13 16:11:10 +0100

    init validator

---
+++ webserv/config/validatorConfig.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validatorConfig.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/13 14:05:52 by pberset           #+#    #+#             */
+/*   Updated: 2026/03/13 14:05:59 by pberset          ###   Lausanne.ch       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "validatorConfig.hpp"
+
+//validate location
+
+bool	validLocRoute(std::string& route);
+bool	validLocRoot(std::string& root);
+void	validLimitExcept(std::vector<std::string>& limitExcept); //default GET is not specified
+void	validUploadPath(std::string& uploadPath); //default /www/upload if not specified. Create if not exists
+//cgi_params ? subject to deletion
+//cgi_pass ? subject to deletion
+
+
+//validate server
+
+void	validServerName(std::string& serverName); //if not set, use "server<port>"
+bool	validServRoot(std::string& root);
+bool	validPort(int port);
+void	validIndex(std::string& index); //if not set, use "index.html"
+void	validAccessLogs(std::string& accessLogs); //create if not exists, can be null
+void	validErrorLogs(std::string& errorLogs); //create if not exists, can be null
+bool	validClientMaxBodySize(unsigned int maxBodySize);
+bool	validErrorPages(std::map<int, std::string>& errorPages);
---
+++ webserv/config/validatorConfig.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validatorConfig.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/13 14:06:05 by pberset           #+#    #+#             */
+/*   Updated: 2026/03/13 14:06:06 by pberset          ###   Lausanne.ch       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VALIDATORCONFIG_HPP
+# define VALIDATORCONFIG_HPP
+
+# include <iostream>
+# include <string>
+# include <vector>
+# include <map>
+
+# include "helperConfig.hpp"
+
+//validate location
+
+bool	validLocRoute(std::string& route);
+bool	validLocRoot(std::string& root);
+void	validLimitExcept(std::vector<std::string>& limitExcept); //default GET is not specified
+void	validUploadPath(std::string& uploadPath); //default /www/upload if not specified. Create if not exists
+//cgi_params ? subject to deletion
+//cgi_pass ? subject to deletion
+
+
+//validate server
+
+void	validServerName(std::string& serverName); //if not set, use "server<port>"
+bool	validServRoot(std::string& root);
+bool	validPort(int port);
+void	validIndex(std::string& index); //if not set, use "index.html"
+void	validAccessLogs(std::string& accessLogs); //create if not exists, can be null
+void	validErrorLogs(std::string& errorLogs); //create if not exists, can be null
+bool	validClientMaxBodySize(unsigned int maxBodySize);
+bool	validErrorPages(std::map<int, std::string>& errorPages);
+
+#endif
