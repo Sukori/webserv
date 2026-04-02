22a996e227a6c31f9bd54927e0d4fb450968a9e5
Yohann Labussiere <ylabussi@c2r7s10.42lausanne.ch>
2026-04-02 20:44:12 +0200

    a bit of doc and fixes

--- webserv/core/utils/ByteString.hpp
+++ webserv/core/utils/ByteString.hpp
@@ -3,8 +3,9 @@
 #include <cstddef>
 #include <cstring>
 #include <iostream>
+#include <string>
 
-typedef char byte;
+typedef unsigned char byte;
 
 class ByteString
 {
@@ -16,48 +17,63 @@ class ByteString
 	ByteString(const ByteString&);
 	~ByteString(void);
 
+	// @brief makes a std::string out of this with no regard for the bytes copied
+	std::string	to_string(void) const;
+
+	// @brief copies contents of ByteString o over to this
 	ByteString&	operator=(const ByteString&);
 
-	size_t		append(const byte*, size_t);
-	size_t		append(const char*);
-	size_t		append(const byte);
-	size_t		append(const ByteString&);
-	const byte*	data(void) const;
+	// @brief appends len bytes from s to this
+	ByteString&	append(const byte*, size_t);
+	// @brief appends c string parameter to this
+	ByteString&	append(const char*);
+	// @brief appends char parameter to this
+	ByteString&	append(const byte);
+	// @brief appends ByteString parameter to this
+	ByteString&	append(const ByteString&);
 
 	const byte&	operator[](size_t) const;
 	byte&		operator[](size_t);
 
+	// @brief true if the string is empty
 	bool		empty(void) const;
+	// @brief getter for the pointer to the bytes
+	const byte*	data(void) const;
+	// @brief getter for the length of the string
 	size_t		length(void) const;
+	// @brief getter for the capacity of the array buffer
 	size_t		capacity(void) const;
 	
 	size_t		reserve(size_t new_cap);
 	size_t		shrink_to_fit(void);
 	void		clear(void);
 
-	ByteString	substr(size_t pos, size_t len = npos);
+	ByteString	substr(size_t pos = 0, size_t len = npos) const;
 
 	size_t		find(const byte*, size_t len, size_t pos = 0) const;
-	size_t		find(byte, size_t pos = 0) const;
-
 	size_t		find_first_not_of(const byte*, size_t len, size_t pos = 0) const;
 	size_t		find_last_not_of(const byte*, size_t len, size_t pos = 0) const;
 	size_t		find_first_of(const byte*, size_t len, size_t pos = 0) const;
 	size_t		find_last_of(const byte*, size_t len, size_t pos = 0) const;
-
 	
 	size_t		find(const char*, size_t pos = 0) const;
 	size_t		find_first_not_of(const char*, size_t pos = 0) const;
 	size_t		find_last_not_of(const char*, size_t pos = 0) const;
 	size_t		find_first_of(const char*, size_t pos = 0) const;
 	size_t		find_last_of(const char*, size_t pos = 0) const;
+	
+	size_t		find(byte, size_t pos = 0) const;
+	size_t		find_first_not_of(byte, size_t pos = 0) const;
+	size_t		find_last_not_of(byte, size_t pos = 0) const;
+	size_t		find_first_of(byte, size_t pos = 0) const;
+	size_t		find_last_of(byte, size_t pos = 0) const;
 
 	static const size_t npos;
 
 	private:
-	byte*	_data;
+	size_t	_cap;
 	size_t	_len;
-	size_t	_cur;
+	byte*	_data;
 };
 
 std::ostream&	operator<<(std::ostream& os, const ByteString&);
--- webserv/core/utils/ByteString_test.cpp
+++ webserv/core/utils/ByteString_test.cpp
@@ -1,27 +1,9 @@
 #include "ByteString.hpp"
-#include <string>
 
 int main(void)
 {
-	ByteString s (20);
-	std::cout << s.capacity() << '\n';
-	std::cout << s.length() << '\n';
-	s.append((const byte*) "hello world", 12);
-	std::cout << s.capacity() << '\n';
-	std::cout << s.length() << '\n';
-	s.shrink_to_fit();
-	std::cout << s.capacity() << '\n';
-	std::cout << s.length() << '\n';
-	std::cout << s << '\n';
-	ByteString s2 (s);
-	std::cout << s2 << '\n';
-	s2.clear();
-	std::cout << s2 << '\n';
-	ByteString s3 ("haiii123f321");
-	std::cout << s3 << '\n';
-	std::cout << s3.substr(s3.find("23")) << '\n';
-	std::cout << s3.substr(s3.find_first_of("f3")) << '\n';
-	std::cout << s3.substr(s3.find_last_not_of("123")) << '\n';
+	ByteString s ("hello");
+	std::cout << s.find('h');
 	return 0;
 }
 
