b7e240592b00fc6af9be5bfb9d3b3ba2ded201c9
neon-05 <yohann.labusiere@gmail.com>
2026-04-16 06:06:50 +0200

    starting to make the cool ass demo website, I started too late ;-;

--- webserv/config/errPages.cpp
+++ webserv/config/errPages.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   errPages.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ylabussi <ylabussi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: neon-05 <neon-05@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 16:29:47 by pberset           #+#    #+#             */
-/*   Updated: 2026/04/15 16:30:06 by ylabussi         ###   ########.fr       */
+/*   Updated: 2026/04/16 03:26:40 by neon-05          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@
 std::map<int, std::string>	defaultErrorPages(void) {
 
 	std::map<int, std::string> m;
-    m[400] = "./www/default_error_pages/400.html";
+    m[400] = "./www/default_error_pages/400.html"; //
     m[401] = "./www/default_error_pages/401.html";
     m[403] = "./www/default_error_pages/403.html";
-    m[404] = "./www/default_error_pages/404.html";
-    m[405] = "./www/default_error_pages/405.html";
-    m[408] = "./www/default_error_pages/408.html";
-    m[413] = "./www/default_error_pages/413.html";
+    m[404] = "./www/default_error_pages/404.html"; //
+    m[405] = "./www/default_error_pages/405.html"; //
+    m[408] = "./www/default_error_pages/408.html"; //
+    m[411] = "./www/default_error_pages/411.html"; //
+    m[413] = "./www/default_error_pages/413.html"; //
     m[500] = "./www/default_error_pages/500.html";
     m[502] = "./www/default_error_pages/502.html";
     m[503] = "./www/default_error_pages/503.html";
